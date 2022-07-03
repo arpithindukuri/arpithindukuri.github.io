c72233672c7acd5a0a2b5f05298ee33898c0d7b2
arpithindukuri <danielarpith@gmail.com>
2022-07-02 22:37:36 -0600

    change page title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import NavBar from "./NavBar";
 
 type Props = {
@@ -7,6 +8,10 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <div className="relative flex h-screen min-h-screen flex-col font-defaultFont">
+      <Head>
+        <title>d1dev.io</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <NavBar />
       <main className="h-full w-full overflow-auto p-4 pb-12">{children}</main>
     </div>
